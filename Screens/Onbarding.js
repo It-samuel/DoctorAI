69cccab68d2877b7ee355f83b0ff6384ab0ce147
it-samuel <samuelhappiness360@gmail.com>
2023-11-23 06:41:07 +0100

    updates

---
+++ Screens/Onbarding.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Onbarding() {
+  return (
+    <View>
+      <Text>Onbarding</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
